9aa40c015b4cde7cd051a42f31d096890746dbff
Lakshdeep Bajwa <bajwals@live.com>
2020-03-07 18:16:21 -0800

    created basic signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Pantry from './Pages/Pantry';
+import Signup from './Pages/Signup'
 import NoMatch from './Pages/NoMatch';
 import {Navbar, NavDropdown} from 'react-bootstrap'
 
@@ -14,12 +15,16 @@ function App() {
           <Navbar.Brand className="mr-auto"><img alt="colored pantry" src="https://image.flaticon.com/icons/svg/1606/1606731.svg" height="25px"></img> Pantry</Navbar.Brand>
 
         <NavDropdown title="Navigation" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/signup">Create an Account</NavDropdown.Item>
             <NavDropdown.Item href="/pantry">My Pantry</NavDropdown.Item>
         </NavDropdown>
       </Navbar>
       <Switch>
         <Route exact path='/pantry'>
           <Pantry />
+        </Route>
+        <Route exact path='/signup'>
+          <Signup />
         </Route>
         <Route>
           <NoMatch />
---
+++ client/src/Pages/Signup.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import 
+{ Row, 
+    Col, 
+    ListGroup, 
+    Button, 
+    Container, 
+    Card, 
+    Form } from 'react-bootstrap'
+
+function Signup () {
+
+
+    return (
+        <Container>
+        <Row>
+        <Col md={6}>
+        <Card className="signup-card">
+        <Card.Title className="text-center py-3">Create an Account</Card.Title>
+        <Form>
+            <ListGroup variant="flush">
+                <ListGroup.Item>
+                    <Form.Control placeholder='@username'></Form.Control>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Form.Row>
+                        <Col>
+                            <Form.Control placeholder="First name" />
+                        </Col>
+                        <Col>
+                            <Form.Control placeholder="Last name" />
+                        </Col>
+                    </Form.Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Form.Control placeholder="password"></Form.Control>
+                </ListGroup.Item>
+            </ListGroup>
+        </Form>
+        </Card>
+        </Col>
+        </Row>
+        </Container>
+    )
+
+
+}
+
+export default Signup
