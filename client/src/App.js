30327bcd3ef3c5b8dbd96d903c661871c49769fc
Benjamin Candy <bcandyman93@gmail.com>
2020-03-09 22:18:27 -0700

    Changed to use 'Link' as opposed to anchor tags to avoid reloading.

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Pantry from './Pages/Pantry';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
 import NoMatch from './Pages/NoMatch';
-import {Navbar, NavDropdown, NavLink} from 'react-bootstrap'
+import { Navbar, NavDropdown } from 'react-bootstrap'
 import './App.css'
 
 function App() {
+
   return (
-  <Router>
+    <Router>
       <Navbar className="mb-3">
         <Navbar.Brand className="mr-auto"><img alt="colored pantry" src="https://image.flaticon.com/icons/svg/1606/1606731.svg" height="25px"></img> Pantry</Navbar.Brand>
         {/* <Navbar.Text>Logged in as: </Navbar.Text> <NavLink href="/profile"> Lakshdeep Bajwa</NavLink> */}
         <NavDropdown title="Navigation" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/signup">Create an Account</NavDropdown.Item>
-            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-            <NavDropdown.Item href="/pantry">My Pantry</NavDropdown.Item>
-            <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
+          <NavDropdown.Item>
+            <Link to='/signup'>Create an Account</Link>
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <Link to='/login'>Login</Link>
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <Link to="/pantry">My Pantry</Link>
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <Link to="/profile">My Profile</Link>
+          </NavDropdown.Item>
         </NavDropdown>
       </Navbar>
       <Switch>
-        <Route exact path='/pantry'>
-          <Pantry />
-        </Route>
-        <Route exact path='/signup'>
-          <Signup />
-        </Route>
-        <Route exact path='/login'>
-          <Login />
-        </Route>
-        <Route exact path='/profile'>
-          <Profile />
-        </Route>
-        <Route>
-          <NoMatch />
-        </Route>
+        <Route exact path='/pantry' component={Pantry} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/profile' component={Profile} />
+        <Route component={NoMatch} />
       </Switch>
       <Navbar fixed="bottom">
         Pantry is a React app created by Ben Candy and Lakshdeep Bajwa
       </Navbar>
-  </Router>
+    </Router>
   )
 }
 
