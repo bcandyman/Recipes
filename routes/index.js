bd78f7283bc686b1c1bdb70ec16d3c53e0f633e9
Benjamin Candy <bcandyman93@gmail.com>
2020-03-14 08:30:34 -0700

    Added dedicatied spoonacular routes file.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const app = require('express').Router();
 
 const userAuthenticationRoutes = require('./userAuthentication');
 const apiRoutes = require('./apiRoutes');
+const spoonacularRoutes = require('./spoonacularRoutes');
 
 app.use('/api/user', userAuthenticationRoutes);
 app.use('/api', apiRoutes);
+app.use('/api', spoonacularRoutes);
 
 module.exports = app;
