65883b28eb996dff16286a575d50e49c2709bbd0
Benjamin Candy <bcandyman93@gmail.com>
2020-03-16 19:02:37 -0700

    Added event listener for when user clicks on recipe to display details.

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -11,15 +11,23 @@ export default function RecipeList({ recipes }) {
     return <Card.Footer className='text-muted'>Ready in {timeInMinutes} minutes</Card.Footer>
   }
 
+  const getDetails = e => {
+    console.log('====================================');
+    console.log(e.currentTarget.getAttribute('data-id'));
+    console.log('====================================');
+  }
+
   return (
     <Row>
       {recipes.map((recipe, index) => (
-        <Col key={index} md={6} data-id={recipe.id}>
-          <Card.Header>
-            <Card.Title className='text-center'>{recipe.title}</Card.Title>
-          </Card.Header>
-          <Card.Img src={recipe.image}></Card.Img>
-          {recipe.readyInMinutes ? preparationTime(recipe.readyInMinutes) : null}
+        <Col onClick={getDetails} key={index} md={6} data-id={recipe.id}>
+          <div>
+            <Card.Header>
+              <Card.Title className='text-center'>{recipe.title}</Card.Title>
+            </Card.Header>
+            <Card.Img src={recipe.image}></Card.Img>
+            {recipe.readyInMinutes ? preparationTime(recipe.readyInMinutes) : null}
+          </div>
         </Col>
       ))}
     </Row>
