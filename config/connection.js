af993efcb8558d7b057c07ecb0ee438114c08613
Benjamin Candy <bcandyman93@gmail.com>
2020-03-18 20:20:02 -0700

    Added configuration for mLab.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/pantry';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://Ben!:Password!20@ds047911.mlab.com:47911/heroku_f15dr7h7';
 
 module.exports = mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
